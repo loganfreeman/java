098f64312fe7fb98200019bfc718b8c3867c4cbe
loganfreeman <loganfreeman@live.com>
2015-07-19 17:57:40 -0600

    all

---
+++ src/main/java/com/scheng/concurrency/intrinsic/ReentrantLockExample.java
@@ -0,0 +1,33 @@
+package com.scheng.concurrency.intrinsic;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by scheng on 7/19/2015.
+ */
+public class ReentrantLockExample {
+	public static void main(String[] args) {
+		Worker worker = new Worker();
+
+		Thread t1 = new Thread(worker, "Thread-1");
+		Thread t2 = new Thread(worker, "Thread-2");
+		t1.start();
+		t2.start();
+	}
+
+	private static class Worker implements Runnable {
+		private final ReentrantLock lock = new ReentrantLock();
+
+		@Override
+		public void run() {
+			lock.lock();
+			try {
+				System.out.println(Thread.currentThread().getName() + " - 1");
+				System.out.println(Thread.currentThread().getName() + " - 2");
+				System.out.println(Thread.currentThread().getName() + " - 3");
+			} finally {
+				lock.unlock();
+			}
+		}
+	}
+}
