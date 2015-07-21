85e672e853e0899d6563ac4cc1310ad220e0cbc8
loganfreeman <loganfreeman@live.com>
2015-07-20 22:20:23 -0600

    all

---
+++ src/main/java/com/scheng/java8/Person.java
@@ -0,0 +1,16 @@
+package com.scheng.java8;
+
+/**
+ * Created by scheng on 7/20/2015.
+ */
+public class Person {
+    public String firstName;
+    public String lastName;
+
+    public Person() {}
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
